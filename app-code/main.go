f52d6dd2d13c90c8060c7b8761227d3e205e26cb
Rania Fahmy <rania.fahmy222@gmail.com>
2023-06-14 12:49:51 +0300

    app modifications

--- app-code/main.go
+++ app-code/main.go
@@ -1,3 +1,18 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"net/http"
+// )
+
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hello World")
+// }
+
+// func main() {
+// 	http.HandleFunc("/", handler)
+// 	http.ListenAndServe(":8000", nil)
+// }
 package main
 
 import (
@@ -6,7 +21,28 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	message := `
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<style>
+				body {
+					background-color: #000;
+					color: #FFF;
+					font-size: 24px;
+					text-align: center;
+					padding-top: 200px;
+				}
+			</style>
+		</head>
+		<body>
+			Hello World! ITI's DevOps Track says Hi
+		</body>
+	</html>
+	`
+
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, message)
 }
 
 func main() {
